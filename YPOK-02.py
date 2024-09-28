b3517bc075d6bda4c490ffca62012a35887c4ead
iuad1fsv <MEXAHuK@>
2024-09-28 13:13:35 +0300

    YPOKu

---
+++ YPOK-02.py
@@ -0,0 +1,38 @@
+# number1 = 10 + 1
+# print(number1)
+# print(type(number))
+# number = 20.6
+# print(number)
+# print(type(number))
+# #number = "KTO TAM"
+# print(number)
+# print(type(number))
+# #number = False
+#
+# ####
+# #number1 = 10
+# print(number)
+# print(number := 10)
+#
+# print(number1 + number)
+
+
+
+# number = 356
+# n1 = number // 100
+# n2 = number % 100 // 10
+# n2_v2 = number // 10 % 10
+# n3 = number % 10
+# print(n1, n2, n3, sep="\n")
+# print(n1 + n2 + n3)
+
+# first_number = int(input("BBEDuTE BTOPOE 4uCJIO: "))
+# second_number = int(input("BBEDuTE BTOPOE 4uCJIO: "))
+# result = first_number + second_number
+# print(result)
+
+name = input("KAK TE69I 3OBYT: ")
+age = int(input("CKOJIbKO TE6E JIET: "))
+# V1
+print("/7PuBET,", name, "BO3PACT", age, "JIET!")
+# v2
