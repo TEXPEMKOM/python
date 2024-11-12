1f5de8ba5b006976fd509838938ec25135e770f3
iuad1fsv <MEXAHuK@>
2024-11-12 11:51:00 +0200

    YPOK-8

--- DOMALLIKA-8.3.py
+++ DOMALLIKA-8.3.py
@@ -33,3 +33,26 @@ assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
 assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
 
 print("ОК")
+
+
+# v2
+import re
+
+def is_palindrome(text):
+    """
+    Функция для проверки, является ли строка палиндромом.
+    :param text: Входная строка.
+    :return: Булево значение True, если строка является палиндромом, иначе False.
+    """
+    # Приводим строку к нижнему регистру и удаляем все неалфавитные символы
+    cleaned_text = re.sub(r'[^a-z0-9]', '', text.lower())
+
+    # Проверяем, является ли очищенная строка палиндромом
+    return cleaned_text == cleaned_text[::-1]
+
+# Тесты
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
