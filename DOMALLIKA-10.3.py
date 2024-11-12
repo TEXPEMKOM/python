c4cfc11785e79146831a93a6faf35d4959020d53
iuad1fsv <MEXAHuK@>
2024-11-12 12:41:46 +0200

    YPOK-10

---
+++ DOMALLIKA-10.3.py
@@ -0,0 +1,27 @@
+# 10.3 Перевірити чи є парним чи ні
+# Ваша функція is_even повинна повертати True якщо число парне, і False якщо число непарне.
+# Вхідні дані: Ціле число.
+# Вихідні дані: Логічний тип.
+#
+# def is_even(digit):
+#     """ Перевірка чи є парним число """
+#     pass
+#
+# assert is_even(2) == True, 'Test1'
+# assert is_even(5) == False, 'Test2'
+# assert is_even(0) == True, 'Test3'
+# print('OK')
+
+def is_even(digit):
+    """
+    Функция для проверки, является ли число четным.
+    :param digit: Целое число.
+    :return: True, если число четное, иначе False.
+    """
+    return digit % 2 == 0
+
+# Тесты
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')
